39af55fd62bd67ee8d9b57a3e0bab9636dafb710
sauravipsator <saurav.kumar@ipsator.com>
2025-01-11 20:52:39 +0530

    SV | fix: import image fixes

--- src/components/constants/manifesto.ts
+++ src/components/constants/manifesto.ts
@@ -1,13 +1,20 @@
+import manifestoExplanation1 from "../../assets/Manifesto/manifesto_explanation1.png"
+import manifestoExplanation2 from "../../assets/Manifesto/manifesto_explanation2.png"
+import manifestoExplanation3 from "../../assets/Manifesto/manifesto_explanation3.png"
+import manifestoExplanation4 from "../../assets/Manifesto/manifesto_explanation4.png"
+import manifestoExplanation5 from "../../assets/Manifesto/manifesto_explanation5.png"
+import manifestoExplanation6 from "../../assets/Manifesto/manifesto_explanation6.png"
+
 export const banner2Contexts=[{description:"I make sure we go above and beyond industry norms in developing our safety and sustainability standards. As a mother, I prefer to err on the side of caution—if there’s any question about an ingredient, we don’t include it.",author:"Jackie, Product Innovation + Insights"},{description:"I make sure we go above and beyond industry norms in developing our safety and sustainability standards. As a mother, I prefer to err on the side of caution—if there’s any question about an ingredient, we don’t include it.",author:"Jackie, Product Innovation + Insights"},{description:"I make sure we go above and beyond industry norms in developing our safety and sustainability standards. As a mother, I prefer to err on the side of caution—if there’s any question about an ingredient, we don’t include it.",author:"Jackie, Product Innovation + Insights"}];
 
 
 export const explanations=[
-    {id:1,title:"Prevention",description:"Our first line of defense is prevention. We install non-slip mats, grab bars, and handrails to minimize the risk of slips and falls, creating a safer environment for your loved ones.",imgSrc:"manifesto_explanation1.png"},
-    {id:2,title:"Interaction",description:"EyEagle’s system seamlessly integrates with the way you and your caregivers interact with your environment. With customizable settings and an easy-to-use app, we ensure that our safety solution works in harmony with your lifestyle.",imgSrc:"manifesto_explanation2.png"},
-    {id:3,title:"Detection",description:"Our system constantly monitors for any signs of trouble. If a fall or emergency occurs, EyEagle’s detection system is immediately activated, ensuring no delay in response.",imgSrc:"manifesto_explanation3.png"},
-    {id:4,title:"Alarm",description:"The moment an incident is detected, the EyEagle alarm system springs into action. Our patented alarm system is designed for maximum reliability, ensuring that help is alerted right away.",imgSrc:"manifesto_explanation4.png"},
-    {id:5,title:"Notification",description:"Once the alarm is triggered, notifications are instantly sent to caregivers and emergency contacts through the EyEagle app, so you’re never in the dark about your loved one’s safety.",imgSrc:"manifesto_explanation5.png"},
-    {id:6,title:"Action",description:"Whether it’s dispatching emergency services or guiding family members through the next steps, EyEagle ensures rapid and decisive action. Our system coordinates the response to make sure help arrives as quickly as possible.",imgSrc:"manifesto_explanation6.png"},
+    {id:1,title:"Prevention",description:"Our first line of defense is prevention. We install non-slip mats, grab bars, and handrails to minimize the risk of slips and falls, creating a safer environment for your loved ones.",imgSrc:manifestoExplanation1},
+    {id:2,title:"Interaction",description:"EyEagle’s system seamlessly integrates with the way you and your caregivers interact with your environment. With customizable settings and an easy-to-use app, we ensure that our safety solution works in harmony with your lifestyle.",imgSrc:manifestoExplanation2},
+    {id:3,title:"Detection",description:"Our system constantly monitors for any signs of trouble. If a fall or emergency occurs, EyEagle’s detection system is immediately activated, ensuring no delay in response.",imgSrc:manifestoExplanation3},
+    {id:4,title:"Alarm",description:"The moment an incident is detected, the EyEagle alarm system springs into action. Our patented alarm system is designed for maximum reliability, ensuring that help is alerted right away.",imgSrc:manifestoExplanation4},
+    {id:5,title:"Notification",description:"Once the alarm is triggered, notifications are instantly sent to caregivers and emergency contacts through the EyEagle app, so you’re never in the dark about your loved one’s safety.",imgSrc:manifestoExplanation5},
+    {id:6,title:"Action",description:"Whether it’s dispatching emergency services or guiding family members through the next steps, EyEagle ensures rapid and decisive action. Our system coordinates the response to make sure help arrives as quickly as possible.",imgSrc:manifestoExplanation6},
 ];
 
 export const contexts = [
