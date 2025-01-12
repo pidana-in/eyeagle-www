c44b8f44adbb1630c4120fbf94b8bc6061e578c0
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2025-01-12 15:48:05 +0530

    SP | feat/bestPlanPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        instrument: ['"Instrument SARIF"', 'sans-serif'],
+    },
+    },
   },
   plugins: [],
 }
