2ea095b721fecbf0fb85e43f8bbf722aa5cec219
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2025-01-12 15:50:36 +0530

    SP | Revert back font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        instrument: ['"Instrument SARIF"', 'sans-serif'],
-    },
     },
   },
   plugins: [],
