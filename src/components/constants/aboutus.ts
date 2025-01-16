d3bdf399ba36eb1778906cec9043c984ce13f58b
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2025-01-16 18:03:57 +0530

    SP | fix: Fixed co-founder name

--- src/components/constants/aboutus.ts
+++ src/components/constants/aboutus.ts
@@ -7,7 +7,7 @@ import Vaibhav from "../../assets/Aboutus/Vaibhav.png";
 export const aboutUsFoundersData=[
     {id:1,name:"Ajay Sharma",title:"Founder",photo:AjaySharma,speech:"“Elder safety isn’t just about preventing accidents; it’s about preserving independence and dignity. At EyEagle, we strive to make every bathroom a safer space, so families can focus on living, not worrying.”"},
     {id:2,name:"Abhinav Kushawaha",title:"Co-founder",photo:Abhinav,speech:"“Our mission is simple: to protect the ones who protected us. With EyEagle, we ensure that every home is equipped with the right tools to keep our loved ones safe in the places where accidents are most likely to happen.”"},
-    {id:3,name:"Ajay Sharma",title:"Co-founder",photo:Gaurav,speech:"“Every detail matters when it comes to elder safety. From the precision of our installations to the speed of our response, EyEagle is committed to ensuring that your loved ones are never left vulnerable.”"},
+    {id:3,name:"Gaurav Kumar",title:"Co-founder",photo:Gaurav,speech:"“Every detail matters when it comes to elder safety. From the precision of our installations to the speed of our response, EyEagle is committed to ensuring that your loved ones are never left vulnerable.”"},
     {id:4,name:"Vaibhav Saxena",title:"Co-founder",photo:Vaibhav,speech:"“Safety for the elderly isn’t just a necessity—it’s a right. At EyEagle, we’re not just installing products; we’re creating peace of mind for families, knowing that their loved ones are protected, even in the most challenging situations.”"}
 ]
 export const Values=[
