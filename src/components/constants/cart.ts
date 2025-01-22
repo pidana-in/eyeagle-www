2bd4067871c069ef24ade578548606d7a6ed950c
sauravipsator <saurav.kumar@ipsator.com>
2025-01-22 14:53:15 +0530

    SV | Feat: Cart page added

---
+++ src/components/constants/cart.ts
@@ -0,0 +1,6 @@
+import productImage from "../../assets/Cart/productImage.png";
+
+export const productData = [
+    {id:1, imageSrc:productImage, title:"Gold Plan - ₹29,999", description:"The big size to stand on it’s own"},
+    {id:2, imageSrc:productImage, title:"Gold Plan - ₹29,999", description:"The big size to stand on it’s own"},
+]
