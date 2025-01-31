c0e10cde7c16ca07f4cdafc240e2a651801e2948
sauravipsator <saurav.kumar@ipsator.com>
2025-01-31 14:25:49 +0530

    SV | Fix: Cart pages and contact pages type constant added

---
+++ src/components/constants/cartDetailItem.ts
@@ -0,0 +1,13 @@
+import OrderItemImage from "../../assets/Orders/OrderItemImage.png";
+
+export const cartDetailItemData = {
+    title:"Kit Details",
+    productData: [
+    {
+    img:OrderItemImage,
+    name:"Gold Plan Kit",
+    description:"The big size to stand on its own",
+    info:"See what’s included"
+    }
+    ]
+}
