5e10a3aac20fc02ca1cef09c694ff879f0bca2f6
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2025-02-06 13:14:56 +0530

    SP | feat/waitlists-page

---
+++ src/components/constants/waitlist.ts
@@ -0,0 +1,20 @@
+export const WaitlistHeaderData={
+    title:`<h1 class="text-[36px] lg:text-[60px] font-medium  text-center"><span class="text-[#CC0000]  max-w-[1200px]">Bringing Safety Home</span> – Be the First to Experience Eyeagle</h1>`,
+description:"Your loved ones deserve the best protection. With Eyeagle, you get instant emergency response, proactive safety features, and complete peace of mind—all in one smart system."
+}
+
+export const WaitlistBannerData={
+    title:"Exclusive Launch Price",
+    price:"$50,000",
+    planDuration:"/ 3 year plan",
+    pointers:[
+        "Priority Installation",
+        "Exclusive Early Benefits",
+        "Be the First to Secure Your Home"
+    ]
+}
+
+export const WaitlistFormData={
+    acceptation:"I want to subscribe to the newsletter",
+    buttonTitile:"Reserve My Safety Now!"
+}
