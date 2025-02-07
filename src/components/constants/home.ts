cdacdb79d4f0261a9a91004d13f008a939194747
sauravipsator <saurav.kumar@ipsator.com>
2025-02-07 10:40:50 +0530

    SV | Fix: Home and Navbar revamp

--- src/components/constants/home.ts
+++ src/components/constants/home.ts
@@ -1,38 +1,57 @@
 import BathroomImage1 from "../../assets/Navbar/bathroomImage1.png";
+import BathroomImage2 from "../../assets/Navbar/bathroomImage2.png";
 import BathroomImage3 from "../../assets/Navbar/bathroomImage3.png";
 import BathroomImage4 from "../../assets/Navbar/bathroomImage4.png";
 import takeCareImage1 from "../../assets/Navbar/takeCareImage1.png";
 import takeCareImage2 from "../../assets/Navbar/takeCareImage2.png";
+import struggle1 from "../../assets/Navbar/struggle1.png";
+import struggle2 from "../../assets/Navbar/struggle2.png";
+import struggle3 from "../../assets/Navbar/struggle3.png";
 
 
 export const bathroomData = [
     {
         id:1,
-        heading:"Overall Bathroom Safety Solution",
+        heading:"Proactive Protection",
         title:"Complete safety overhaul of bathroom",
         description:"Our customized solutions cover all aspects of bathroom safety, from non-slip mats to handrails and grab bars. EyEagle provides everything needed for a safer environment, reducing the chances of accidents.",
         image:BathroomImage1
     },
     {
         id:2,
-        heading:"Patented alarm system",
+        heading:"Advanced Fall Dection",
         title:"Our PIDANA Alarm device that lets you help in an emergency",
         description:"In case of an emergency, our patented alarm system ensures rapid alerts. When the alarm is triggered, notifications are immediately sent to the EyEagle app, informing caregivers or family members about the situation.",
-        image:BathroomImage1
+        image:BathroomImage2
     },
     {
         id:3,
-        heading:"Eyeagle app",
+        heading:"Realtime Alerts",
         title:"Comprehensive App that provides quick alerts & help",
         description:"Once the alarm is raised, caregivers are instantly alerted on their smartphones. Whether you’re at home or miles away, EyEagle ensures that help is just a notification away.",
         image:BathroomImage3
     },
     {
         id:4,
-        heading:"Trained professional team",
+        heading:"Realtime Alerts",
         title:"Established and trained team to help battle emergency",
         description:"If needed, EyEagle’s integrated response system can even dispatch emergency services, ensuring your loved ones receive help as soon as possible.",
         image:BathroomImage4
+    },
+];
+
+export const struggleData = [
+    {
+        img:struggle1,
+        content:`1 in 4 seniors falls each<span class="text-[#CC0000]"> year— 37% in the bathroom</span>`,
+    },
+    {
+        img:struggle2,
+        content:`<span>95%</span> of hip fractures are from falls, many in bathrooms.`,
+    },
+    {
+        img:struggle3,
+        content:`<span>10-15%</span> of bathroom falls lead to hospitalization.`,
     },
 ]
 
@@ -60,14 +79,15 @@ export const takeCareData = [
     },
 ];
 
-export const heroDescription = "Prevent, Don’t Regret A single fall can change it all";
+export const heroDescription = `<span class="text-[#CC0000]">PREVENT</span>, DON'T <span class="text-[#CC0000]">REGRET</span> A SINGLE FALL CAN <span class="text-[#CC0000]">CHANGE</span> IT ALL`;
 
-export const healthOrganization1 = "According to the World Health Organization, millions of elderly individuals, including our parents, experience falls each year, many leading to serious injury or hospitalization.";
-export const healthOrganization2 = "But it doesn't have to be this way.";
+export const healthOrganization1 = `The Silent Crisis Affecting <span class="text-[#CC0000]">Millions like You</span>`;
+export const healthOrganization2 = `Falls aren't just accidents;
+they're <span class="text-[#CC0000] font-bold">life-altering</span> events for seniors.`;
 
-export const productFeatureTitle = `The most <span class="text-red-600">complete bathroom</span> safety you've ever seen`;
+export const productFeatureTitle = `With advanced monitoring, real-time alerts, and professional intervention, Eyeagle isn’t just a device—it’s a 24/7 guardian for your loved ones, providing peace of mind no matter where you are.`;
 
-export const ourTeamHeading1 = "Our complete bathroom safety kit addresses every risk, giving you and your family the peace of mind that they’re safe";
+export const ourTeamHeading1 = "World Class Prevention System";
 
 export const ourTeamHeading2 = " Our trained professionals are available 24/7 to ensure the safety of your loved ones.";
 
@@ -79,6 +99,6 @@ export const desireSection1 = "Your Loved Ones Deserve the Best.";
 
 export const desireSection2 = "Don’t wait for the worst to happen—be proactive and protect your loved ones with EyEagle’s complete safety kit.";
 
-export const descriptionButton = "Get Started";
+export const descriptionButton = "Join waiting list";
 
 
