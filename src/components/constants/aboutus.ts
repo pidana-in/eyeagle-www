6f8e01592419f39c727b54ecc2980d5b280d2fa4
SuneetPanigrahi <suneet.panigrahi@ipsator.com>
2025-02-21 20:41:29 +0530

    SP | fix: refactored code

--- src/components/constants/aboutus.ts
+++ src/components/constants/aboutus.ts
@@ -2,13 +2,16 @@ import AjaySharma from "../../assets/Aboutus/AjaySharma.png";
 import Abhinav from "../../assets/Aboutus/Abhinav.png";
 import Gaurav from "../../assets/Aboutus/Gaurav.png";
 import Vaibhav from "../../assets/Aboutus/Vaibhav.png";
+import HeaderBanner from "../../assets/Aboutus/HeaderBanner.png";
+import HeaderBanner2 from "../../assets/Aboutus/HeaderBanner2.png";
 
 
 export const aboutUsFoundersData=[
-    {id:1,name:"Ajay Sharma",title:"Founder",photo:AjaySharma,speech:"“Elder safety isn’t just about preventing accidents; it’s about preserving independence and dignity. At EyEagle, we strive to make every bathroom a safer space, so families can focus on living, not worrying.”"},
-    {id:2,name:"Abhinav Kushawaha",title:"Co-founder",photo:Abhinav,speech:"“Our mission is simple: to protect the ones who protected us. With EyEagle, we ensure that every home is equipped with the right tools to keep our loved ones safe in the places where accidents are most likely to happen.”"},
-    {id:3,name:"Gaurav Kumar",title:"Co-founder",photo:Gaurav,speech:"“Every detail matters when it comes to elder safety. From the precision of our installations to the speed of our response, EyEagle is committed to ensuring that your loved ones are never left vulnerable.”"},
-    {id:4,name:"Vaibhav Saxena",title:"Co-founder",photo:Vaibhav,speech:"“Safety for the elderly isn’t just a necessity—it’s a right. At EyEagle, we’re not just installing products; we’re creating peace of mind for families, knowing that their loved ones are protected, even in the most challenging situations.”"}
+   
+    {id:1,name:"Ajay Sharma",title:"Founder",experience:"Tech Enthusiast, IIT, Preventing accidents, Building safety",photo:AjaySharma,speech:"“Elder safety isn’t just about preventing accidents; it’s about preserving independence and dignity. At EyEagle, we strive to make every bathroom a safer space, so families can focus on living, not worrying.”"},
+    {id:2,name:"Abhinav Kushawaha",title:"Co-founder",experience:"Tech Enthusiast, IIT, Preventing accidents, Building safety",photo:Abhinav,speech:"“Our mission is simple: to protect the ones who protected us. With EyEagle, we ensure that every home is equipped with the right tools to keep our loved ones safe in the places where accidents are most likely to happen.”"},
+    {id:3,name:"Gaurav Kumar",title:"Co-founder",experience:"Tech Enthusiast, IIT, Preventing accidents, Building safety",photo:Gaurav,speech:"“Every detail matters when it comes to elder safety. From the precision of our installations to the speed of our response, EyEagle is committed to ensuring that your loved ones are never left vulnerable.”"},
+    {id:4,name:"Vaibhav Saxena",title:"Co-founder",experience:"Tech Enthusiast, IIT, Preventing accidents, Building safety",photo:Vaibhav,speech:"“Safety for the elderly isn’t just a necessity—it’s a right. At EyEagle, we’re not just installing products; we’re creating peace of mind for families, knowing that their loved ones are protected, even in the most challenging situations.”"}
 ]
 export const Values=[
     {title:"Our Values",description:"The safety system activates to contain and manage the situation. Alarms will sound and alerts will be sent to ensure immediate response. The system is designed to minimize damage and ensure safety. 4o mini"},
@@ -16,3 +19,29 @@ export const Values=[
     {title:"Reliability",description:"We are dedicated to delivering a reliable and effective product that exceeds customer expectations."},
     {title:"Customer Focus",description:"We prioritize the needs and satisfaction of our customers, always putting their well-being first."}
 ]
+
+export const AboutUsHeaderData={headerBannerSrc:HeaderBanner,headerBannerSrc2:HeaderBanner2,title:`From <span class="text-[#CC0000]">Prevention</span> to <span class="text-[#CC0000]">Protection</span>, <br>We’re with You`,desc:`<span class="text-[#CC0000]">Empowering families</span> to care for their elderly loved ones, no matter where they are. Inspired by real challenges faced by seniors and their families, <span class="text-[#CC0000]">EyEagle bridges the gap between technology and compassionate care.</span>`}
+
+
+export const CoreValuesData={
+    title:`At <span class="text-[#CC0000]">EyEagle</span>, we’re redefining elder safety.`,
+    desc:"We are committed to redefining elder safety with cutting-edge security systems, emergency alarms, and our Circle app membership. We strive to provide unmatched protection and peace of mind—ensuring safety doesn’t come at an overwhelming cost.",
+    coreTitle:`Guided by Our <span class="text-[#CC0000]">Core Values</span>`,
+    values:[
+       { title:"Innovation",desc:"We are committed to staying at the forefront of technology and innovation to develop cutting-edge solutions for senior safety."},
+       { title:"Reliability",desc:"We are dedicated to delivering a reliable and effective product that exceeds customer expectations."},
+       { title:"Customer Focus",desc:"We prioritize the needs and satisfaction of our customers, always putting their well-being first."},
+
+    ]
+}
+
+export const EyeagleDiff={
+    title:`Why <span class="text-[#CC0000]">EyEagle</span> is different?`,
+    content:[
+        {iconType:"star",title:"Best-in-class attachments for ultimate safety",desc:"Our installments and attachments are crafted from premium materials to ensure durability, reliability and longevity."},
+        {iconType:"app_promo",title:"The power of the Circle app",desc:"Stay connected, no matter where you are. The Circle App integrates seamlessly with our fall alert devices. This ensures caregivers and families receive real-time notifications of emergencies or unusual activity."},
+        {iconType:"currency_exchange",title:"Cost-effective peace of mind",desc:"Handling an emergency from miles away can be costly and time-consuming. EyEagle’s emergency alert devices for the elderly offer instant assistance at a fraction of the expense."},
+        {iconType:"emergency_home",title:"Reliable emergency response",desc:"EyEagle’s panic alarm systems ensure swift action in emergencies. Linked to emergency services, they provide rapid response when every second counts. Our 24/7 support team ensures no call for help goes unanswered."},
+    ]
+
+}
