48a3467f01a981959024c9dfded5e2d1e5f8ef1d
sauravipsator <saurav.kumar@ipsator.com>
2025-02-25 16:51:17 +0530

    SV | Fix: home

--- src/components/constants/home.ts
+++ src/components/constants/home.ts
@@ -9,6 +9,7 @@ import struggle1 from "../../assets/Navbar/struggle1.png";
 import struggle2 from "../../assets/Navbar/struggle2.png";
 import struggle3 from "../../assets/Navbar/struggle3.png";
 import circleApp from "../../assets/Home/circleApp.png";
+import circleAppMobile from "../../assets/Home/circleAppMobile.png";
 import powerfulBento from "../../assets/Home/powerfulBento.png";
 import powerfulFeat1 from "../../assets/Home/powerfulFeat1.png";
 import powerfulFeat2 from "../../assets/Home/powerfulFeat2.png";
@@ -50,11 +51,11 @@ export const bathroomData = [
 export const struggleData = [
     {
         img:struggle1,
-        content:`Every year <span class="text-[#CC0000]">1 in 4 senior </span> fall in the bathroom`,
+        content:`Every year <span class="text-[#CC0000]">1 in 4 seniors </span> fall in the bathroom`,
     },
     {
         img:struggle2,
-        content:`<span class="text-[#CC0000]">95% of hip fractures</span> in seniors are caused by falls in bathroom`,
+        content:`<span class="text-[#CC0000]">95% of hip fractures</span> in seniors are caused by falls in the bathroom`,
     },
     {
         img:struggle3,
@@ -89,11 +90,11 @@ export const takeCareData = [
 export const heroDescription = `<span class="text-[#CC0000]">PREVENT</span>, DON'T <span class="text-[#CC0000]">REGRET</span>`;
 export const heroDescription2 = `A SINGLE FALL CAN <span class="text-[#CC0000]">CHANGE</span> IT ALL`
 
-export const healthOrganization1 = `The Silent Crisis <br>Affecting <br><span class="text-[#CC0000]">Millions like You</span>`;
+export const healthOrganization1 = `The Silent Crisis <br>Affecting <br><span class="text-[#CC0000]">Millions Like You</span>`;
 export const healthOrganization2 = `Falls aren't just accidents;
 they're <span class="text-[#CC0000] font-bold">life-altering</span> events for seniors.`;
 
-export const productFeatureTitle = `With advanced monitoring, real-time alerts, and professional intervention, Eyeagle isn’t just a device—it’s a 24/7 complete guardian for your loved ones, providing peace of mind no matter where you are.`;
+export const productFeatureTitle = `With advanced monitoring, real-time alerts, and professional intervention, EyEagle is a 24/7 guardian for your loved ones, providing peace of mind no matter where you are.`;
 
 export const ourTeamHeading1 = "World Class <br> Prevention System";
 
@@ -107,7 +108,7 @@ export const desireSection1 = "Get EyEagle now – Stay One Step Ahead of Emerge
 
 export const desireSection2 = "Don’t wait for the worst to happen—be proactive and protect your loved ones with EyEagle’s complete safety kit.";
 
-export const descriptionButton = "Join waiting list";
+export const descriptionButton = "Join waitlist";
 
 export const scrollDataSection = {1:"prevention",2:"services",3:"emergency",4:"installation"};
 
@@ -117,13 +118,13 @@ export const EmergencyResponseData={
     title:"ACTION",
     subTitle:"Emergency Response, <br>Anytime, Anywhere",
     midTitle:`Trained <span class="text-[#CC0000]">responders, medical professionals</span>, and specialists — ready to act when every second counts.`,
-desc:"If caregivers don’t respond, Eyeagle’s emergency team steps in. Our experts arrive on-site, assess the situation, and take immediate action—whether it’s providing urgent care or coordinating with doctors and hospitals. With access to critical documents, they ensure fast, informed decisions for the best care.",
-bannerDesc1:`“A flight home takes <span class="text-[#CC0000]">30 hours.<span/> <span class="text-white">EyEagle team takes</span> <span class="text-[#CC0000]">30 minutes</span>”`,
-bannerDesc2:`“because some things can wait. <span class="text-[#CC0000]">Emergencies</span> can’t. Eyeagle knows the difference.”`,
+desc:"If caregivers don’t respond, EyEagle’s emergency team steps in. Our experts arrive on-site, assess the situation, and take immediate action—whether it’s providing urgent care or coordinating with doctors and hospitals. They ensure fast, informed decisions for the best care.",
+bannerDesc1:`“A flight home takes <span class="font-semibold">30 hours.<span/> <span class="text-white font-normal">EyEagle team takes</span> <span class="font-semibold">30 minutes</span>”`,
+bannerDesc2:`“because some things can wait. <span class="text-[#CC0000]">Emergencies</span> can’t. EyEagle knows the difference.”`,
 remarks:[
     {iconType:"help_clinic",context:"24/7 guidance & support"},
     {iconType:"support_agent",context:"Provide support in absence of caregivers"},
-    {iconType:"help_clinic",context:"Free Ambulance Services"},
+    {iconType:"help_clinic",context:"Ambulance Services"},
     {iconType:"e911_emergency",context:"Quick Alert during emergency"}
 ],
 }
@@ -135,21 +136,21 @@ export const HandleSectionData={
     imgSrc:HomePageHandleBanner,
     instructions:[
         {title:"Full Installation",desc:"From grab bars and anti-slip mats to the alarm kit, we set up everything for maximum safety."},
-        {title:"Personalized Safety Audit",desc:" We assess your bathroom and recommend the best safety upgrades."},
-        {title:"Seamless App Onboarding",desc:" Connect your Circle and customize alerts with ease."},
-        {title:"Thorough Testing",desc:" We ensure every device and alert system works flawlessly."},
+        {title:"Personalized Safety Audit",desc:"We assess your bathroom and recommend the best safety upgrades."},
+        {title:"Seamless App Onboarding",desc:"Connect your Circle and customize alerts with ease."},
+        {title:"Thorough Testing",desc:"We ensure every device and alert system works flawlessly."},
 
     ]
 }
 
 export const CircleMembershipData={
     title:"APP",
-    subTItle1:"EyEagle Circle",
-    subTItle2:"App Membership",
-    midTitle:"Introducing EyEagle Circle – Connected Care Network for<br> Your Loved Ones",
-    desc:"EyEagle Circle is a smart, connected network linking caregivers and family members to provide continuous safety and support.",
-    imgSrc:circleApp
-
+    subTItle1:"EyEagle Circles",
+    subTItle2:"Device-App Membership",
+    midTitle:`Introducing EyEagle Circles – <br/> Connected Care Network for Your Loved Ones`,
+    desc:"EyEagle Circles is a smart, connected network linking caregivers and family members to provide continuous safety and support.",
+    imgSrc:circleApp,
+    imgSrcMobile:circleAppMobile
 }
 
 export const HeroImageTitle={
@@ -171,8 +172,10 @@ featsrc5:powerfulFeat2Mobile,
 export const HomeFaqsData={title:"FAQs",content:[
     {title:"What is EyEagle and how does it work?",desc:"EyEagle is a comprehensive bathroom safety system designed to prevent accidents and provide quick emergency response. It includes a patented alarm system, non-slip surfaces, grab bars, and an app that sends instant notifications to caregivers or emergency contacts when an incident is detected."},
     {title:"Do I need Wi-Fi for the system to work?",desc:"No, EyEagle does not rely on Wi-Fi. It operates independently to ensure that safety is never compromised due to internet connectivity issues."},
+    {title:"How does Eyeagle ensure notifications are reliable?",desc:"Our patent-pending device is designed to function in any corner of the house, even on low-bandwidth 2G networks. This ensures that critical alerts are always delivered, even in areas with poor connectivity."},
     {title:"Can I customize the settings for my bathroom?",desc:"Yes! EyEagle offers customizable settings that allow you to adjust alarm sensitivity, notification preferences, and system configurations to match the layout and specific needs of your bathroom"},
     {title:"What happens if there’s a power outage?",desc:"Alerts can be sent to multiple people, including caregivers, family members, or emergency services. You can customize who gets notified through the EyEagle app."},
-    {title:"Who receives the alerts when the alarm is triggered?",desc:"EyEagle comes equipped with a battery that lasts up to 60 hours, ensuring continuous protection even during power outages."},
+    {title:"Who receives the alerts when the alarm is triggered?",desc:"EyEagle comes equipped with a battery that lasts up to 6 hours, ensuring continuous protection even during power outages."},
     {title:"How is the system installed?",desc:"Our team of experts will install the system based on your bathroom’s specific floor plan. We ensure everything is placed optimally for maximum safety and effectiveness."},
+    {title:"What if my device goes offline?",desc:"If a device disconnects, you’ll receive a “Device Offline” alert, prompting you to check power and connectivity."}
     ]}
