f3aa07a5ea1e1702977e0db76d05c6c2f6b4ca13
navdeepIP <navdeep.nagar@ipsator.com>
2025-05-14 09:28:37 +0000

    NN | Fix: cards issue in bathroom section

--- src/components/constants/home.ts
+++ src/components/constants/home.ts
@@ -1,5 +1,5 @@
 import BathroomImage1new from "../../assets/Navbar/bathroomImage1new.png";
-import FrameNew from "../../assets/Navbar/FrameNew.jpg";
+import Frame7 from "../../assets/Navbar/Frame7.jpg";
 import BathroomImage3new from "../../assets/Navbar/bathroomImage3new.png";
 import BathroomImage4new from "../../assets/Navbar/bathroomImage4new.png";
 import takeCareImage1 from "../../assets/Navbar/takeCareImage1.png";
@@ -32,7 +32,7 @@ export const bathroomData = [
     title: "Our PIDANA Alarm device that lets you help in an emergency",
     description:
       "In case of an emergency, our patented alarm system ensures rapid alerts. When the alarm is triggered, notifications are immediately sent to the EyEagle app, informing caregivers or family members about the situation.",
-    image: FrameNew,
+    image: Frame7,
   },
   {
     id: 3,
