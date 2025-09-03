3045789305328b7a3b5d282e44ad116b93798062
Abhinav Kushwaha <abhinav@ipsator.com>
2025-09-03 21:20:39 +0530

    added shopify api

--- src/components/constants/waitlist.ts
+++ src/components/constants/waitlist.ts
@@ -1,38 +1,34 @@
-export const WaitlistHeaderData={
-    title:`<h1 class="text-[36px] lg:text-[60px] font-medium  text-center"><span class="text-[#CC0000]  max-w-[1200px]">Bringing Safety Home</span> – Be the First to Experience EyEagle</h1>`,
-description:"Your loved ones deserve the best protection. With EyEagle, you get instant emergency response, proactive safety features, and complete peace of mind—all in one smart system."
-}
+export const WaitlistHeaderData = {
+  title: `<h1 class="text-[36px] lg:text-[60px] font-medium  text-center"><span class="text-[#CC0000]  max-w-[1200px]">Bringing Safety Home</span> – Be the First to Experience EyEagle</h1>`,
+  description: "Your loved ones deserve the best protection. With EyEagle, you get instant emergency response, proactive safety features, and complete peace of mind—all in one smart system.",
+};
 
-export const WaitlistBannerData={
-    title:"Exclusive Launch Price",
-    price:"₹49,999",
-    planDuration:"/ 3 year plan",
-    pointers:[
-        "Priority Installation",
-        "Exclusive Early Benefits",
-        "Be the First to Secure Your Home"
-    ]
-}
+export const WaitlistBannerData = {
+  title: "Exclusive Launch Price",
+  price: "₹49,999",
+  planDuration: "/ 3 year plan",
+  pointers: ["Priority Installation", "Exclusive Early Benefits", "Be the First to Secure Your Home"],
+};
 
-export const WaitlistFormData={
-    acceptation:"I want to subscribe to the newsletter",
-    buttonTitile:"Reserve My Safety Now!"
-}
+export const WaitlistFormData = {
+  acceptation: "I want to subscribe to the newsletter",
+  buttonTitile: "Reserve My Safety Now!",
+};
 
-export const postFormFillData={
-    floatingText:"SPECIAL LAUNCH OFFER",
-    boxContent:"For the next 100 people",
-    title:`<span class="text-[#CC0000]">Less</span> than your <span class="text-[#CC0000]">emergency</span> flight ticket`,
-    mobileTtle:`For Less than your <span class="text-[#CC0000]">emergency</span> flight ticket`,
-    cutPrice:` + <span class="line-through">9999</span>  <span  class="text-[#CC0000] text-[24px] font-medium leading-7 mb-[-20px]">₹1</span> `,
-originalPrice:"₹9,998",
-duration:"/ 1 year",
-    contacts:[
-        {iconType:"check",title:"Full bathroom upgrade and support"},
-        {iconType:"check",title:"Patent-pending device-app System"},
-        {iconType:"check",title:"12 months Circles membership worth ₹9999"},
-        {iconType:"check",title:"Ambulance & Emergency Services"},
-    ],
-    btnText:"Reserve my kit",
-    shippingText:"Shipping Starts This September! Secure your order today"
-}
+export const postFormFillData = {
+  floatingText: "SPECIAL LAUNCH OFFER",
+  boxContent: "For the next 100 people",
+  title: `<span class="text-[#CC0000]">Less</span> than your <span class="text-[#CC0000]">emergency</span> flight ticket`,
+  mobileTtle: `For Less than your <span class="text-[#CC0000]">emergency</span> flight ticket`,
+  cutPrice: ` + <span class="line-through">29,999</span>`,
+  originalPrice: "₹19,999",
+  duration: "",
+  contacts: [
+    { iconType: "check", title: "Safety audit of each bathroom before dispatch and on-site during installation" },
+    { iconType: "check", title: "Anti-skid mats, Grab bars & Grip tapes to protect against falls" },
+    { iconType: "check", title: "EyEagle Circles App subscription of 1 year" },
+    { iconType: "check", title: "1 Ambulance cover for the year" },
+  ],
+  btnText: "Reserve my kit",
+  shippingText: "Secure your order today",
+};
