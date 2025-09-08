c4d18f549fc6952cd8fc287cbd68d39690852752
navdeepIP <navdeep.nagar@ipsator.com>
2025-09-08 06:27:14 +0000

    NN | Fix: Updated waitlist form

--- src/components/constants/waitlist.ts
+++ src/components/constants/waitlist.ts
@@ -1,6 +1,6 @@
 export const WaitlistHeaderData = {
-  title: `<h1 class="text-[36px] lg:text-[60px] font-medium  text-center"><span class="text-[#CC0000]  max-w-[1200px]">Bringing Safety Home</span> – Be the First to Experience EyEagle</h1>`,
-  description: "Your loved ones deserve the best protection. With EyEagle, you get instant emergency response, proactive safety features, and complete peace of mind—all in one smart system.",
+  title: `Be the First to Experience the EyEagle Guardian-X Kit`,
+  description: "Patent-pending, seamlessly integrated safety system. Sign up to get early access, updates, and exclusive offers.",
 };
 
 export const WaitlistBannerData = {
