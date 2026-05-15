79c48c38043722c4f514eeb4932082e1ca806e71
Navdeep Nagar <navdeep.nagar@ipsator.com>
2026-05-15 18:18:05 +0530

    NN | Fix: Upgraded packages to Astro 6.3

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,20 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const blogs = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/blogs",
+  }),
+
   schema: ({ image }) =>
     z.object({
       id: z.coerce.number(),
       title: z.string(),
+      slug: z.string().optional(),
       category: z.string(),
       img: image(),
-      slug: z.string().optional(),
       author: z.string(),
       authorRole: z.string(),
       date: z.coerce.date(),
