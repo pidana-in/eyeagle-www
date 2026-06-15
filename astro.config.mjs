ce176f3852b80bfbf48d494295a20e55a7266e64
Navdeep Nagar <navdeep.nagar@ipsator.com>
2026-06-15 17:19:24 +0530

    NN | Fix: Added XML sitemap and removed some old pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,17 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://eyeagle.ai/",
-
   output: "server",
-
   adapter: netlify(),
-
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
-
+    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
